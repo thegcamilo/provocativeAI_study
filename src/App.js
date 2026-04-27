c23f4c6d0e2c99669c7418d7696b60d9a243fdfd
Gabriel Lima <gcamilo7@gmail.com>
2026-04-27 15:32:13 +0200

    scale points:

--- src/App.js
+++ src/App.js
@@ -247,7 +247,7 @@ class App extends Component {
       <div className="scale-row">
         <div className="scale-anchor anchor-left">{leftAnchor}</div>
         <div className="scale-options-group">
-          {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((num) => (
+          {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((num) => (
             <label key={num} className="scale-unit">
               <input 
                 type="radio" 
